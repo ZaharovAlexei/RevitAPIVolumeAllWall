d42e60406f04c5b68385b9ce4baf0800476847b7
Алексей Захаров <zakharov.nnov@gmail.com>
2022-02-02 00:33:59 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIVolumeAllWall
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var walls = new FilteredElementCollector(doc)
+                .OfClass(typeof(Wall))
+                .Cast<Wall>()
+                .ToList();
+
+            double wallsVolume = 0;
+            foreach (Wall wall in walls)
+            {
+                Parameter volumeParameter = wall.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED);
+                double volumeValue = UnitUtils.ConvertFromInternalUnits(volumeParameter.AsDouble(), UnitTypeId.CubicMeters);
+                wallsVolume += volumeValue;
+            }
+
+            TaskDialog.Show("Wall info", $"Объем стен в проекте: {wallsVolume:F2} м³");
+
+            return Result.Succeeded;
+        }
+    }
+}
